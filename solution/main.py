ff494ca871ea3612f6adce6f3f30597411303e23
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-19 11:14:49 +0530

    Update main.py

--- solution/main.py
+++ solution/main.py
@@ -2,24 +2,15 @@
 number_of_cases = 0
 numbers_list = []
 
-# TODO
-def read_case_count():
-    pass
 
-
-# TODO
-def read_n():
-    pass
-
-
-# TODO
-def read_numbers(n):
+def read_input(n):
     file1 = open('sample-io/sudoku_checker_sample_ts1_inputs.txt', 'r')
-    
+    lines = file1.readlines()
+    for line in lines:
+        for i in range(0, n):
+            numbers_list.append(int(line(i)))
+    print(numbers_list)
 
 
 if __name__ == '__main__':
-    number_of_cases = read_case_count()
-    for case in range(number_of_cases):
-        n = read_n()
-        read_numbers(n)
+    read_input
