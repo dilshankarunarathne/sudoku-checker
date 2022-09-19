4e8627ee0b0fcdf8261ad4d0454972fb3bab367b
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-19 11:12:52 +0530

    Update main.py

--- solution/main.py
+++ solution/main.py
@@ -1,5 +1,25 @@
 
+number_of_cases = 0
+numbers_list = []
 
-if __name__ == '__main__':
+# TODO
+def read_case_count():
+    pass
+
+
+# TODO
+def read_n():
+    pass
+
+
+# TODO
+def read_numbers(n):
+    file1 = open('sample-io/sudoku_checker_sample_ts1_inputs.txt', 'r')
     
 
+
+if __name__ == '__main__':
+    number_of_cases = read_case_count()
+    for case in range(number_of_cases):
+        n = read_n()
+        read_numbers(n)
