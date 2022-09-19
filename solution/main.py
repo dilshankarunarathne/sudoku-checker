eabf3a21f3248acd336ce336a6a0c929957cbfed
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-19 11:17:04 +0530

    Update main.py

--- solution/main.py
+++ solution/main.py
@@ -1,16 +1,19 @@
-
-number_of_cases = 0
 numbers_list = []
 
 
 def read_input(n):
     file1 = open('sample-io/sudoku_checker_sample_ts1_inputs.txt', 'r')
     lines = file1.readlines()
+    n = 0
 
     for line in lines:
         number_of_cases = int(lines[0])
+        numbers_list.append(line.strip())
         for i in range(0, n):
-            numbers_list.append(int(line(i)))
+            numbers_list.append(int(line.strip(i)))
+
+        n = number_of_cases
+
     print(numbers_list)
 
 
