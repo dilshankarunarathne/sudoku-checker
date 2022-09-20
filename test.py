79555eeaa75b27d5e569c8076de28da2ba0aec7e
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-20 12:38:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 data = []
 
-f = open("test.txt", "r")
-print(f.read())
-f.close()
+with open('test.txt', 'r') as f:
+    for line in f:
 
-# with open('test.txt', 'r') as f:
-#     for line in f:
-#
-#         # Read the number of cases
-#         number_of_cases = int(f.readline())
-#
-#         # For each case...
-#         for i in range(number_of_cases):
-#             # Read N
-#             n = int(f.readline())
-#
-#             # For N lines...
-#             for j in range(n):
-#                 # Read one line
-#                 line = f.readline().split()
-#                 print (line)
+        # Read the number of cases
+        number_of_cases = int(f.readline())
+
+        # For each case...
+        for i in range(number_of_cases):
+            # Read N
+            n = int(f.readline())
+
+            # For N lines...
+            for j in range(n):
+                # Read one line
+                line = f.readline().split()
+                print (line)
 
