96c5f85007b77928f82fe09071b4687f6c9c55af
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-20 13:09:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,20 +6,20 @@ with open('test.txt', 'r') as f:
 
     # Read the number of cases
     number_of_cases = int(f.readline())
-    print('num cases = ', number_of_cases)
-
+    # print('num cases = ', number_of_cases) # DEBUG
+    
     for case in range(number_of_cases):
         # ==== READ DATA ===
 
         # Read N for case
         n = int(f.readline())
-        print('n = ', n)
+        # print('n = ', n) # DEBUG
 
         # For N*N times
         for line in range(n*n):
             # Read the line
             lines.append(f.readline().split())
-            print('line = ', lines[line])
+            # print('line = ', lines[line]) # DEBUG
 
         # ==== PROCESS DATA ===
 
