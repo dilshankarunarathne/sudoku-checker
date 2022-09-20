cf9ac34189e96ef81c0794c1b7c7ced445335b4e
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-09-20 12:32:39 +0530

    Update test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+data = []
+
+with open('test.txt') as f:
+    for line in f:
+
+        # Read the number of cases
+        number_of_cases = int(f.readline())
+
+        # For each case...
+
+        # Read N
+
